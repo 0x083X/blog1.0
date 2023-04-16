e1f09d5e49325b5205409400438c30b13904aebe
0x083X <115934135+0x083X@users.noreply.github.com>
2023-04-16 18:44:56 +0800

    Update axios.ts

--- bloghoutai/webfront/src/server/axios.ts
+++ bloghoutai/webfront/src/server/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://43.139.155.210:3000'
+axios.defaults.baseURL = 'http://localhost:3000'
 axios.defaults.timeout = 10000
 
 export default axios
